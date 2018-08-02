f8644976f3d3d3d9b9311d8a848c9434974540fc
vafang25 <vivianfang25@gmail.com>
2018-08-02 15:35:49 -0400

    design changes

---
+++ web.js
@@ -0,0 +1,39 @@
+function bigImg(x){
+    x.style.height = "350px";
+    x.style.width = "350px";
+}
+
+function norImg(x) {
+    x.style.height = "240px";
+    x.style.width = "240px";
+}
+
+function bigPic(x){
+    x.style.height = "350px";
+    x.style.width = "350px";
+}
+
+function norPic(x) {
+    x.style.height = "240px";
+    x.style.width = "240px";
+}
+
+function bigPho(x){
+    x.style.height = "350px";
+    x.style.width = "350px";
+}
+
+function norPho(x) {
+    x.style.height = "240px";
+    x.style.width = "240px";
+}
+
+function bigVis(x){
+    x.style.height = "350px";
+    x.style.width = "350px";
+}
+
+function norVis(x) {
+    x.style.height = "240px";
+    x.style.width = "240px";
+}
